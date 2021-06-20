f143578cb514e654da54dc0a110e54d8331af779
Veehaab <78991443+Veehaab@users.noreply.github.com>
2021-06-19 19:17:10 -0700

    Add files via upload

---
+++ ingedrence.js
@@ -0,0 +1,19 @@
+2 teaspoons salt
+½ cup cooking oil
+1 ½ cups chopped onion
+1 tablespoon minced garlic
+1 ½ teaspoons minced fresh ginger root
+1 tablespoon curry powder
+1 teaspoon ground cumin
+1 teaspoon ground turmeric
+1 teaspoon ground coriander
+1 teaspoon cayenne pepper
+1 tablespoon water
+1 (15 ounce) can crushed tomatoes
+1 cup plain yogurt
+1 tablespoon chopped fresh cilantro
+1 teaspoon salt
+½ cup water
+1 teaspoon garam masala
+1 tablespoon chopped fresh cilantro
+1 tablespoon fresh lemon juice
